45f45c42dbdfe049ded431162e82cec37a5713f3
Katrina Moody <kmfoster212@gmail.com>
2025-04-09 19:30:24 -0700

    Created function to loop through all buttons and display content in input field

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// loop through each button and add an event listener that,
+//  when clicked, shows the number/symbol on the button as the input's value
+let buttons = document.querySelectorAll("button");
+let input = document.getElementById('input');
 
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+       input.value += button.textContent ; // shortcut to append results, not replace
+    });
+});
+
+//You should store the content of the display
+// (the number) in a variable for use in the next step
+
+// Create a new function operate that takes an operator and two numbers
+//  and then calls one of the above functions on the numbers.
