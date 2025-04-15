67eefde39f726cb674fe18eeb9dfb70e6215aadf
Katrina Moody <kmfoster212@gmail.com>
2025-04-14 20:25:00 -0400

    Added more logic to the eventListener functions.

--- script.js
+++ script.js
@@ -3,21 +3,29 @@
 const numButtons = document.querySelectorAll(".num");
 const opsButtons = document.querySelectorAll(".ops")
 const input = document.getElementById('input');
-let previousValue = 0; // variable that will save previous value in input field
+let currentValue = ''; // variable to represent current value displayed
+let previousValue = ''; // variable that will save previous value in input field
+const operator = this.dataset.operator; // for buttons given a data-operator attribute
 
+// TO GET THE BUTTONS' "CONTENT" TO SHOW IN THE INPUT
 
 numButtons.forEach(button => {
     button.addEventListener("click", function () {
-       input.value += button.textContent ; // shortcut to append results, not replace
-
+    let numValue = parseInt(button.value); // convert button value to a number
+       input.value += numValue ; // shortcut to append results, not replace
     });
 });
 
 opsButtons.forEach(button => {
     button.addEventListener("click", function () {
      previousValue = input.value;
+     console.log(previousValue); // confirms it does display the previous value
+     
      input.value = button.textContent; // replaces input value with ops buttons
 
-       console.log(previousValue); // confirms it does display the previous value
+       
     });
 });
+
+// when operator is pressed, it is appended to the preivous value
+
